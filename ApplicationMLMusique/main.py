e134089d614ec29d5722a8abcd37c18c6bcee21d
Soorry <77273729+Soorry@users.noreply.github.com>
2024-02-10 00:29:51 +0100

    Add lm & fix mlp

--- ApplicationMLMusique/main.py
+++ ApplicationMLMusique/main.py
@@ -2,42 +2,130 @@ import array
 import ctypes
 import numpy as np
 from PIL import Image
+import matplotlib.pyplot as plt
 
-class MultiLayerPerceptron(ctypes.Structure):
-    _fields_ = [
-        ("n_entree", ctypes.c_size_t),
-        ("n_sortie", ctypes.c_size_t),
-        ("n_hidden", ctypes.c_size_t),
-        ("n_poids", ctypes.c_size_t),
-        ("poids", ctypes.POINTER(ctypes.c_double)),
-        ("entrees", ctypes.POINTER(ctypes.c_double)),
-        ("sorties", ctypes.POINTER(ctypes.c_double)),
-    ]
 
 # Chargement d'une image
-image_path = r'C:\Users\HP\Desktop\Projets ESGI\ML_RVJV5_Dug_Zhe_You\image.png'
-image = Image.open(image_path)
+# image_path = r'C:\Users\HP\Desktop\Projets ESGI\ML_RVJV5_Dug_Zhe_You\image.png'
+# image = Image.open(image_path)
 
-largeur, hauteur = image.size
-pixels = list(image.getdata())
-print(pixels[:10])
-image.close()
+# largeur, hauteur = image.size
+# pixels = list(image.getdata())
+# print(pixels[:10])
+# image.close()
+
 
 # Chargement de la dll
-my_dll = ctypes.CDLL(r'C:\Users\HP\Desktop\Projets ESGI\ML_RVJV5_Dug_Zhe_You\MLlib\target\debug\MLlib.dll')
-create_mlp = my_dll.create_mlp
-create_mlp.argtypes = [ctypes.c_size_t, ctypes.c_size_t, ctypes.c_size_t]
-create_mlp.restype = ctypes.POINTER(MultiLayerPerceptron)
+my_dll = ctypes.CDLL(r'C:\Users\HP\Desktop\Projets ESGI\ML_RVJV5_Dug_Zhe_You\MLlib\target\release\MLlib.dll')
 
-mlp_ptr = create_mlp(2, 2, 1)
+isLm = False
 
-mlp = mlp_ptr.contents
+if isLm:
+    # config lm
+    my_dll.create_lm.restype = ctypes.POINTER(ctypes.c_void_p)
+    my_dll.train_lm.restype = ctypes.POINTER(ctypes.c_double)
+    my_dll.predict_lm.restype = ctypes.c_double
 
-# Exemple du xor sous la forme (x0, x1, y)
-data = [1.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 0.0, -1.0, 1.0, 1.0, -1.0]
-float_array = np.array(data, dtype=np.float64)
+    model = my_dll.create_lm()
 
-my_dll.mlpLearning(mlp_ptr, float_array.ctypes.data_as(ctypes.POINTER(ctypes.c_double)), len(float_array))
-free_mlp = my_dll.free_mlp
-free_mlp.argtypes = [ctypes.POINTER(MultiLayerPerceptron)]
-free_mlp(mlp_ptr)
+    X = np.concatenate(
+        [np.random.random((50, 2)) * 0.9 + np.array([1, 1]), np.random.random((50, 2)) * 0.9 + np.array([2, 2])])
+    Y = np.concatenate([np.ones((50, 1)), np.ones((50, 1)) * -1.0])
+
+    train_input_data_pointer = X.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+    train_output_data_pointer = Y.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+
+    nb_errors = 1000
+    errs = my_dll.train_lm(model, ctypes.c_double(0.001), 100000, nb_errors, train_input_data_pointer, len(X), len(X[0]),
+                        train_output_data_pointer, len(Y))
+    array_from_ptr = np.frombuffer(ctypes.cast(errs, ctypes.POINTER(ctypes.c_double * nb_errors)).contents, dtype=np.float64)
+    plt.plot(range(0, nb_errors), array_from_ptr)
+    plt.show()
+
+    outputs = []
+    for x, y in X:
+        data = np.array([x, y], dtype=np.float64)
+        predict_data_pointer = data.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+        result = my_dll.predict_lm(model, predict_data_pointer, len(data))
+        outputs.append(result)
+
+    inputs = np.array(X)
+    outputs = np.array(outputs)
+
+    plt.scatter(inputs[outputs >= 0][:, 0], inputs[outputs >= 0][:, 1], color='red')
+    plt.scatter(inputs[outputs < 0][:, 0], inputs[outputs < 0][:, 1], color='blue')
+    plt.show()
+    plt.clf()
+else:
+    # config mlp
+    my_dll.create_mlp.restype = ctypes.POINTER(ctypes.c_void_p)
+    my_dll.train_mlp.restype = ctypes.POINTER(ctypes.c_double)
+    my_dll.predict_mlp.restype = ctypes.POINTER(ctypes.c_double)
+
+    model = my_dll.create_mlp(5, 6, 1)
+
+    #XOR
+    #model = my_dll.create_mlp(2, 2, 1)
+    #xor_input = np.array([[0.0, 0.0], [1.0, 0.0], [0.0, 1.0], [1.0, 1.0]], dtype=np.float64)
+    #xor_output = np.array([[-1.0], [1.0], [1.0], [-1.0]], dtype=np.float64)
+
+    #Cross
+    #model = my_dll.create_mlp(3, 4, 1) #alpha 0.001, iter 10000000
+    #X = np.random.random((400, 2)) * 2.0 - 1.0
+    #Y = np.array([[1.0] if abs(p[0]) <= 0.3 or abs(p[1]) <= 0.3 else [-1.0] for p in X], dtype=np.float64)
+
+    #MultiLinear
+    #model = my_dll.create_mlp(2, 2, 1) #alpha 0.01, iter 50000
+    #X = np.random.random((500, 2)) * 2.0 - 1.0
+    #Y = np.array([[1.0] if -p[0] - p[1] - 0.5 > 0 and p[1] < 0 and p[0] - p[1] - 0.5 < 0 else
+                  #[0.0] if -p[0] - p[1] - 0.5 < 0 and p[1] > 0 and p[0] - p[1] - 0.5 < 0 else
+                  #[-1.0] if -p[0] - p[1] - 0.5 < 0 and p[1] < 0 and p[0] - p[1] - 0.5 > 0 else
+                  #[2] for p in X])
+    #X = X[[not np.all(arr == [2]) for arr in Y]]
+    #Y = Y[[not np.all(arr == [2]) for arr in Y]]
+
+    X = np.random.random((1000, 2)) * 2.0 - 1.0
+    Y = np.array([[-1.0] if abs(p[0] % 0.5) <= 0.25 and abs(p[1] % 0.5) > 0.25 else [0.0] if abs(
+        p[0] % 0.5) > 0.25 and abs(p[1] % 0.5) <= 0.25 else [1.0] for p in X])
+
+    train_input_data_pointer = X.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+    train_output_data_pointer = Y.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+
+    nb_err = 1000
+    errs = my_dll.train_mlp(model, ctypes.c_double(0.001), 50000, nb_err, train_input_data_pointer, len(X), len(X[0]), train_output_data_pointer, len(Y), len(Y[0]))
+
+    array_from_ptr = np.frombuffer(ctypes.cast(errs, ctypes.POINTER(ctypes.c_double * nb_err)).contents, dtype=np.float64)
+
+    plt.plot(range(0, nb_err), array_from_ptr)
+    plt.show()
+
+    # Créer un tableau d'entrées
+    inputs = []
+    outputs = []
+    """
+    for x in np.arange(-1, 1, 0.1):
+        for y in np.arange(-1, 1, 0.1):
+            data = np.array([x, y], dtype=np.float64)
+            predict_data_pointer = data.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+            result = my_dll.predict_mlp(model, predict_data_pointer, len(data))
+            inputs.append([x, y])
+            outputs.append(result[0])
+    # Convertir les tableaux en numpy arrays pour une utilisation facile avec Matplotlib
+    inputs = np.array(inputs)
+    outputs = np.array(outputs)
+    """
+    """
+    for x, y in X:
+        data = np.array([x, y], dtype=np.float64)
+        predict_data_pointer = data.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+        result = my_dll.predict_mlp(model, predict_data_pointer, len(data))
+        outputs.append(result[0])
+    """
+    inputs = np.array(X)
+    outputs = np.array(Y)
+
+    plt.scatter(inputs[outputs >= 0.5][:, 0], inputs[outputs >= 0.5][:, 1], color='red')
+    plt.scatter(inputs[outputs < -0.5][:, 0], inputs[outputs < -0.5][:, 1], color='blue')
+    plt.scatter(inputs[(outputs > -0.5) & (outputs < 0.5)][:, 0], inputs[(outputs > -0.5) & (outputs < 0.5)][:, 1], color='green')
+    plt.show()
+    plt.clf()
--- MLlib/src/lib.rs
+++ MLlib/src/lib.rs
@@ -1,298 +1,262 @@
 use rand::Rng;
-use audio_visualizer::waveform::png_file::waveform_static_png_visualize;
-use audio_visualizer::ChannelInterleavement;
-use audio_visualizer::Channels;
-use minimp3::{Decoder as Mp3Decoder, Error as Mp3Error, Frame as Mp3Frame};
-use std::fs::File;
-use std::path::PathBuf;
 
-fn main() {
-    let mut path = PathBuf::new();
-    path.push("test/out");
-    path.push("01.mp3");
-    let mut decoder = Mp3Decoder::new(File::open(path).unwrap());
+struct LinearModel {
+    poids: Vec<f64>,
+}
 
-    let mut lrlr_mp3_samples = vec![];
-    loop {
-        match decoder.next_frame() {
-            Ok(Mp3Frame {
-                data: samples_of_frame,
-                ..
-            }) => {
-                for sample in samples_of_frame {
-                    lrlr_mp3_samples.push(sample);
+impl LinearModel {
+    fn new() -> Self {
+        LinearModel { poids : vec![0.0;3] }
+    }
+
+	fn initPoids(&mut self) {
+		for j in 0..self.poids.len() {
+			self.poids[j] = rand::thread_rng().gen_range(-1.0..1.0);
+		}
+	}
+
+    fn predict(&self, x1: f64, x2: f64) -> f64 {
+        self.poids[0] + self.poids[1] * x1 + self.poids[2] * x2
+    }
+
+    // Fonction d'entraînement pour le modèle linéaire
+    fn train(&mut self, all_inputs: Vec<Vec<f64>>, all_expected_outputs: Vec<f64>, alpha: f64, max_iter: usize, nb_errors : usize) -> Vec<f64> {
+		let err_mod = (max_iter/nb_errors) as usize;
+		let mut errors: Vec<f64> = Vec::new();
+		for iter in 0..max_iter {
+			if(iter % err_mod == 0) {
+				errors.push(self.calculate_error(&all_inputs, &all_expected_outputs));
+			}
+            let k = rand::thread_rng().gen_range(0..all_inputs.len());
+            let inputs_k = &all_inputs[k];
+            let Yk = &all_expected_outputs[k];
+			
+			let mut Xk = vec![1.0;3];
+			Xk[1] = inputs_k[0];
+			Xk[2] = inputs_k[1];
+
+			let yPredict = self.predict(Xk[1], Xk[2]);
+
+			//let gXk = if yPredict >= 0.0 { 1.0 } else { -1.0 };
+
+			// Mettez à jour les poids W
+			self.poids[0] += alpha * (Yk - yPredict);
+			self.poids[1] += alpha * (Yk - yPredict) * Xk[1];
+			self.poids[2] += alpha * (Yk - yPredict) * Xk[2];
+		}
+		errors
+    }
+	
+	fn calculate_error(&mut self, all_inputs: &Vec<Vec<f64>>, all_expected_outputs: &Vec<f64>) -> f64{
+		let mut total_loss = 0.0;
+		for iter in 0..all_inputs.len() {
+			let result = self.predict(all_inputs[iter][0],all_inputs[iter][1]);
+			total_loss += (all_expected_outputs[iter] - result).abs();
+		}
+		total_loss/(all_inputs.len() as f64)
+	}
+}
+
+#[no_mangle]
+pub extern "C" fn create_lm(nb_entree: i32, nb_couche: i32, nb_sortie: i32) -> *mut LinearModel {
+	let mut LinearModel = Box::new(LinearModel::new());
+	LinearModel.initPoids();
+    Box::leak(LinearModel)
+}
+
+#[no_mangle]
+pub extern "C" fn predict_lm(ptr: *mut LinearModel, input: *const f64, length: i32) -> f64 {
+    let model = unsafe { &mut *ptr };
+    let slice = unsafe { std::slice::from_raw_parts(input, length as usize) };
+    let result = model.predict(slice[0], slice[1]);
+	result
+}
+
+#[no_mangle]
+pub extern "C" fn train_lm(ptr: *mut LinearModel, alpha : f64, max_iter : i32,  nb_errors : i32, input : *const f64, rowsIn: i32, colsIn: i32, output: *const f64, rowsOut: i32) -> *mut f64 {
+    let model = unsafe { &mut *ptr };
+    let sliceIn = unsafe { std::slice::from_raw_parts(input, (rowsIn * colsIn) as usize) };
+    let mut input_data = vec![vec![0.0; colsIn as usize]; rowsIn as usize];
+    for i in 0..rowsIn {
+        let start = (i * colsIn) as usize;
+        let end = ((i + 1) * colsIn) as usize;
+        let row = &sliceIn[start..end];
+        input_data[i as usize] = row.to_vec();
+    }
+
+	let output_data = unsafe { std::slice::from_raw_parts(output, rowsOut as usize) };
+    (model.train(input_data, output_data.to_vec(), alpha, max_iter as usize, nb_errors as usize)).leak().as_mut_ptr()
+}
+
+pub struct MLP {
+    n_entree: usize,
+    n_sortie: usize,
+    n_couche: usize,
+    poids: Vec<Vec<Vec<f64>>>,
+    entrees: Vec<Vec<f64>>,
+    deltas: Vec<Vec<f64>>,
+}
+
+impl MLP {
+	fn new(mut nb_entree: usize, mut nb_couche: usize, nb_sortie: usize) -> Self {
+		nb_entree+=1;
+		nb_couche+=1;
+        Self {
+			n_entree: nb_entree,
+			n_sortie: nb_sortie,
+			n_couche: nb_couche,
+			poids: vec![vec![vec![0.0;nb_entree]; nb_entree]; nb_couche],
+			entrees: vec![vec![1.0;nb_entree]; nb_couche],
+			deltas: vec![vec![0.0;nb_entree]; nb_couche],
+        }
+    }
+	
+	fn initPoids(&mut self) {
+		for l in 0..self.n_couche {
+			for i in 0..self.n_entree {
+				for j in 0..self.n_entree {
+					self.poids[l][i][j] = rand::thread_rng().gen_range(-1.0..1.0);
+				}
+			}
+		}
+	}
+
+    fn propagate(&mut self, inputs: Vec<f64>, is_classification: bool) {
+        //assert_eq!(inputs.len(), self.n_entree-1, "La taille des entrées ne correspond pas au nombre d'entrées du modèle");
+
+        for j in 0..inputs.len() {
+            self.entrees[0][j] = inputs[j];
+        }
+
+        for l in 1..self.n_couche {
+            for j in 0..self.n_entree-1 {
+                let mut sum = 0.0;
+
+                for i in 0..self.n_entree {
+                    sum += self.poids[l][i][j] * self.entrees[l - 1][i];
+                }
+
+                if l < self.n_couche - 1 || is_classification {
+                    sum = sum.tanh();
+                }
+
+                self.entrees[l][j] = sum;
+            }
+        }
+    }
+
+    pub fn predict(&mut self, inputs: Vec<f64>, is_classification: bool) -> Vec<f64> {
+        self.propagate(inputs, is_classification);
+        self.entrees[self.n_couche-1].clone()
+    }
+
+    pub fn train(&mut self, all_inputs: Vec<Vec<f64>>, all_expected_outputs: Vec<Vec<f64>>, is_classification: bool, alpha: f64, max_iter: usize, nb_errors : usize) -> Vec<f64> {
+		let mut errs: Vec<f64> = Vec::new();
+		let err_mod = (max_iter/nb_errors) as usize;
+		for iter in 0..max_iter {	
+			if(iter % err_mod == 0){
+				errs.push(self.calculate_loss(&all_inputs, &all_expected_outputs));
+			}
+			
+            let k = rand::thread_rng().gen_range(0..all_inputs.len());
+            let inputs_k = &all_inputs[k];
+            let expected_outputs = &all_expected_outputs[k];
+
+            self.propagate(inputs_k.to_vec(), is_classification);
+			/*
+			if((self.entrees[self.n_couche-1][0] - expected_outputs[0]).abs() < 0.5){
+				continue;
+			}
+			*/
+            for j in 0..self.n_sortie {
+                self.deltas[self.n_couche - 1][j] = self.entrees[self.n_couche - 1][j] - expected_outputs[j];
+
+                if is_classification {
+                    self.deltas[self.n_couche - 1][j] *= 1.0 - self.entrees[self.n_couche - 1][j].powi(2);
                 }
             }
-            Err(Mp3Error::Eof) => break,
-            Err(e) => panic!("{:?}", e),
-        }
-    }
 
-    waveform_static_png_visualize(
-        &lrlr_mp3_samples,
-        Channels::Stereo(ChannelInterleavement::LRLR),
-        "test/out",
-        "sample_1_waveform.png",
-    );
-}
+            for l in (1..self.n_couche).rev() {
+                for i in 0..self.n_entree {
+                    let mut sum = 0.0;
 
-pub struct MultiLayerPerceptron {
-    n_entree : usize,
-    n_sortie : usize,
-    n_hidden : usize,
-    poids : Vec<f64>,
-    entrees : Vec<f64>,
-    sorties : Vec<f64>,
-    n_poids : usize,
-}
-
-impl MultiLayerPerceptron {
-    fn new() -> MultiLayerPerceptron {
-        MultiLayerPerceptron {
-            n_entree: 0,
-            n_sortie: 0,
-            n_hidden: 0,
-            poids: Vec::new(),
-            entrees: Vec::new(),
-            sorties: Vec::new(),
-            n_poids: 0,
-        }
-    }
-}
-
-
-#[no_mangle]
-pub extern "C" fn free_mlp(mlp_ptr: *mut MultiLayerPerceptron) {
-    unsafe {
-        Box::from_raw(mlp_ptr);
-    }
-}
-
-#[no_mangle]
-pub extern "C" fn create_mlp(nb_entree: usize, nb_hidden: usize, nb_sortie: usize) -> *mut MultiLayerPerceptron {
-	let mlp = Box::new(init_lin_mod_internal(nb_entree, nb_hidden, nb_sortie));
-    Box::into_raw(mlp)
-}
-
-	/*
-#[no_mangle]
-pub extern "C" fn main(nb_entree: usize, nb_hidden: usize, nb_sortie: usize) -> *mut MultiLayerPerceptron {
-    // Créez une nouvelle instance de MultiLayerPerceptron
-    let mut mlp = MultiLayerPerceptron::new();
-
-    // Utilisez la fonction init_lin_mod pour initialiser le mlp
-    let mut mlp_ptr: *mut MultiLayerPerceptron = &mut mlp;
-	init_lin_mod(&mut mlp_ptr, nb_entree, nb_hidden, nb_sortie);
-
-    // Retournez le pointeur vers le MultiLayerPerceptron initialisé
-    mlp_ptr
-}
-*/
-#[no_mangle]
-pub extern "C" fn init_lin_mod(mlp_ptr: *mut *mut MultiLayerPerceptron, nb_entree: usize, nb_hidden: usize, nb_sortie: usize) {
-    unsafe {
-        *mlp_ptr = Box::into_raw(Box::new(init_lin_mod_internal(nb_entree, nb_hidden, nb_sortie)));
-    }
-}
-
-pub extern "C" fn init_lin_mod_internal(nb_entree: usize, nb_hidden: usize, nb_sortie: usize) -> MultiLayerPerceptron {
-    let mut lm = MultiLayerPerceptron {
-        n_entree: nb_entree + 1,
-        n_sortie: nb_sortie,
-        n_hidden: nb_hidden,
-        poids: vec![0.0;(nb_entree + 1) * (nb_hidden) + (nb_entree + 1)],
-		entrees: vec![1.0;(nb_entree + 1) * (nb_hidden)],
-        sorties: vec![0.0; nb_sortie],
-        n_poids: (nb_entree + 1) * (nb_hidden) + (nb_entree + 1),
-    };
-
-	//println!("entrees {}, sortie {}, poids {} ",lm.entrees.len(), lm.sorties.len(),lm.poids.len());
-
-	//Création des poids de façon aléatoire
-    for p in 0..lm.n_poids {
-        lm.poids[p] = rand::thread_rng().gen_range(-1.0..1.0);
-    }
-	
-	//Test avec les poids du XOR
-	// lm.poids[0] = 1.0;
-	// lm.poids[1] = 1.0;
-	// lm.poids[2] = -0.5;
-	// lm.poids[3] = -1.0;
-	// lm.poids[4] = -1.0;
-	// lm.poids[5] = 1.5;
-	// lm.poids[6] = 1.0;
-	// lm.poids[7] = 1.0;
-	// lm.poids[8] = -1.5;
-	
-	/*
-	for i in 0..lm.n_poids {
-        println!(" {} ",lm.poids[i]);
-    }
-	*/
-    lm
-}
-
-//Fonction utilisée pour être appelé par le python
-#[no_mangle]
-pub extern "C" fn ask_lin_mod<'a>(
-    mlp: *mut MultiLayerPerceptron,
-    test_data: *mut f64,
-    data_len: usize,
-) -> *mut MultiLayerPerceptron { 
-	// Convertir le pointeur brut vers la structure Rust
-    let mut mlp = unsafe { &mut *mlp };
-
-    // Convertir le pointeur brut vers les données f64 en un slice
-    let test_data_slice = unsafe { std::slice::from_raw_parts(test_data, data_len) };
-
-    // Appeler la fonction avec les paramètres convertis
-    mlp = ask_lin_mod_internal(mlp, test_data_slice);
-
-    // Convertir la référence mutable en pointeur brut pour renvoyer
-    mlp as *mut MultiLayerPerceptron
-}
-
-#[no_mangle]
-fn ask_lin_mod_internal<'a>(
-    mlp: &'a mut MultiLayerPerceptron,
-    test_data: &'a [f64],
-) -> &'a mut MultiLayerPerceptron {
-	
-	//On ajoute les entrées au MLP
-	for indexE in 0..mlp.n_entree-1 {
-        mlp.entrees[indexE] = test_data[indexE];
-    }
-
-	//On calcul les output intermédiaire pour chaque couche
-    for indexH in 0..mlp.n_hidden-1 { //Index des couches
-        for indexE in 0..mlp.n_entree-1 { //Index de la 2eme colonne de perceptron 
-            let mut sum : f64 = 0.0;
-            let indexEntree = indexH * mlp.n_entree;
-            let indexPoids = indexH * mlp.n_entree + indexE * mlp.n_entree;
-			//println!("{} {}", indexEntree, indexPoids);
-
-            for indexN in 0..mlp.n_entree-1 { //Index de la 1ere colonne de perceptron 
-                sum += mlp.entrees[indexEntree + indexN] * mlp.poids[indexPoids + indexN];
-            }
-            sum += mlp.poids[indexPoids + mlp.n_entree-1];
-            let res = 1.0/(1.0 + libm::exp(-sum));
-			//println!("res {}, index {}", res, (indexH+1) * mlp.n_entree + indexE);
-            mlp.entrees[(indexH+1) * mlp.n_entree + indexE] = res;
-        }
-    }
-
-	//Affichage
-	// for i in 0..mlp.entrees.len() {
-		// println!("i {} : {}",i, mlp.entrees[i]);
-	// }
-
-	println!("x0 : {}, x1 : {}", test_data[0], test_data[1]);
-	
-	//On calcul les sorties
-    for indexS in 0..mlp.n_sortie { //Index de la sortie
-        let mut sum : f64 = 0.0;
-        let indexEntree = (mlp.n_hidden-1) * mlp.n_entree;
-        let indexPoids = (mlp.n_hidden-1) * mlp.n_entree + indexS * mlp.n_entree;
-        for indexN in 0..mlp.n_entree-1 { //Index de la 1ere colonne de perceptron 
-            sum += mlp.entrees[indexEntree + indexN] * mlp.poids[indexPoids + indexN];
-        }
-        sum += mlp.poids[indexPoids + mlp.n_entree-1];
-        let res = 1.0/(1.0 + libm::exp(-sum));
-        mlp.sorties[indexS] = res;
-		println!("res : {}",res);
-    }
-    mlp
-}
-
-//Fonction utilisée pour être appelé par le python
-#[no_mangle]
-pub extern "C" fn mlpLearning<'a>(
-    mlp: *mut MultiLayerPerceptron,
-    test_data: *mut f64,
-    data_len: usize)
-{
-	// Convertir le pointeur brut vers la structure Rust
-    let mut mlp = unsafe { &mut *mlp };
-
-    // Convertir le pointeur brut vers les données f64 en un slice
-    let test_data_slice = unsafe { std::slice::from_raw_parts(test_data, data_len) };
-
-    // Appeler la fonction avec les paramètres convertis
-    mlpLearning_internal(mlp, test_data_slice);
-}
-
-//Entrainement du MLP
-#[no_mangle]
-pub extern "C" fn mlpLearning_internal<'a>(mut mlp: &'a mut MultiLayerPerceptron, learn_data: &'a [f64]) {
-	let alpha = 0.1;
-	//La structure utilisé est un seul tableau avec les input et output les un à la suite des autres
-	//Il faut donc calculer la taille et le nombre des blocs en fonctions du nombre d'entrée et de sortie
-    let blockSize = mlp.n_entree - 1 + mlp.n_sortie;
-    let nbBlocks = learn_data.len() / blockSize;
-	
-    let mut rng = rand::thread_rng();
-    for learningTime in 0..1000 {
-		//On choisis un exemple au hasard
-        let randBlockIndex = rng.gen_range(0..nbBlocks-1) * blockSize;
-		//On sépare les output et les input
-        let mut test_data : &'a [f64] = &learn_data[randBlockIndex..randBlockIndex+mlp.n_entree-1];
-        let mut res_data: &'a [f64] = &learn_data[randBlockIndex+mlp.n_entree-1..randBlockIndex+mlp.n_entree+mlp.n_sortie-1];
-		//On passe l'exemple dans le mlp
-        mlp = ask_lin_mod_internal(mlp, test_data);
-		println!("expect : {}",res_data[0]);
-		println!(" ");
-		let res = &mlp.sorties;
-        let mut isOk = true;
-		//On check si la sortie est correct
-        for nbSortie in 0..mlp.n_sortie {
-            if (res[nbSortie] - res_data[nbSortie]).abs() > 1e-6 {
-                isOk = false;
-            }
-        }
-        if !isOk {
-			//Si ce n'est pas le cas on corrige
-            let mut deltas =  vec![0.0;4]; 
-			//on calcule les deltas de la derniere couche
-            for indexE in 0..mlp.n_sortie { //Index de la sortie
-                let res = (1.0 - mlp.sorties[indexE]*mlp.sorties[indexE]) * (mlp.sorties[indexE] - res_data[0]);
-                deltas[3] = res;
-            }
-			//On calcul le reste des deltas a partir de l avant derniere couche
-            for indexH in (0..mlp.n_hidden-1).rev() { //Index des couches
-                for indexE in 0..mlp.n_entree { //Index de la 2eme colonne de perceptron 
-                    let mut sum : f64 = 0.0;
-                    let indexDelta = indexH * mlp.n_entree + indexE;
-					let indexNextDelta = (indexH+1) * mlp.n_entree;
-                    let indexPoids = indexH * mlp.n_entree + indexE * mlp.n_entree;
-                    for indexN in 0..mlp.n_entree { //Index de la 1ere colonne de perceptron 
-                        sum += deltas[3] * mlp.poids[indexPoids + indexN];
+                    for j in 0..self.n_entree {
+                        sum += self.poids[l][i][j] * self.deltas[l][j];
                     }
-                    let res = (1.0 - mlp.entrees[indexE]*mlp.entrees[indexE]) * sum;
-                    deltas[indexDelta] = res;
-					//println!("delta nb : {}, resd  {}", indexDelta, res);
+
+                    self.deltas[l - 1][i] = (1.0 - self.entrees[l - 1][i].powi(2)) * sum;
                 }
             }
-			
-			// for i in 0..deltas.len() {
-				// println!("i {} : {}",i, deltas[i]);
-			// }
-			
-			//On met a jour les poids
-			// println!("timer : {} ",learningTime); 
-			for indexH in 0..mlp.n_hidden-1 {
-                for indexI in 0..mlp.n_entree {
-                    for indexJ in 0..mlp.n_entree {
-                        let indexPoids = indexH * mlp.n_entree * mlp.n_entree + indexI * mlp.n_entree + indexJ;
-                        let indexEntree = indexH * mlp.n_entree + indexI;
-                        let mut indexDelta = indexI;
-						if(indexH > 0) {
-							indexDelta = 3;
-						}
-                        mlp.poids[indexPoids] = mlp.poids[indexPoids] - alpha * mlp.entrees[indexEntree] * deltas[indexDelta];
-						// println!("nb poids {}, val  {} ",indexPoids,mlp.poids[indexPoids]);
+
+            for l in 1..self.n_couche {
+                for i in 0..self.n_entree {
+                    for j in 0..self.n_entree {
+                        self.poids[l][i][j] -= alpha * self.entrees[l - 1][i] * self.deltas[l][j];
                     }
                 }
             }
+			/*
+			if(iter == max_iter-1){
+				for l in 0..self.n_couche {
+					for j in 0..self.n_entree {
+						println!("{}, {}, {}", l, j, self.deltas[l][j]);
+					}
+				}
+			}*/
         }
+
+		errs
     }
+	
+	fn calculate_loss(&mut self, inputs: &Vec<Vec<f64>>, targets: &Vec<Vec<f64>>) -> f64 {
+		let mut total_loss = 0.0;
+		for (input, target) in inputs.iter().zip(targets.iter()) {
+			let prediction = self.predict(input.to_vec(), true);
+			//println!("{}", prediction[0]);
+			let errors: Vec<f64> = prediction.iter().zip(target.iter()).map(|(p, t)| t - p).collect();
+			total_loss += errors.iter().map(|e| e.powi(2)).sum::<f64>();
+		}
+
+		total_loss / (inputs.len() as f64)
+	}
+}
+
+#[no_mangle]
+pub extern "C" fn create_mlp(nb_entree: i32, nb_couche: i32, nb_sortie: i32) -> *mut MLP {
+	let mut mlp = Box::new(MLP::new(nb_entree as usize, nb_couche as usize, nb_sortie as usize));
+	mlp.initPoids();
+    Box::leak(mlp)
+}
+
+#[no_mangle]
+pub extern "C" fn predict_mlp(ptr: *mut MLP, input: *const f64, length: i32) -> *mut f64 {
+    let model = unsafe { &mut *ptr };
+    let slice = unsafe { std::slice::from_raw_parts(input, length as usize) };
+    let result = model.predict(slice.to_vec(), true);
+    result.leak().as_mut_ptr()
+}
+
+#[no_mangle]
+pub extern "C" fn train_mlp(ptr: *mut MLP, alpha : f64, max_iter : i32, nb_errors : i32, input : *const f64, rowsIn: i32, colsIn: i32, output: *const f64, rowsOut: i32, colsOut: i32) -> *mut f64 {
+    let model = unsafe { &mut *ptr };
+    let sliceIn = unsafe { std::slice::from_raw_parts(input, (rowsIn * colsIn) as usize) };
+    let mut input_data = vec![vec![0.0; colsIn as usize]; rowsIn as usize];
+    for i in 0..rowsIn {
+        let start = (i * colsIn) as usize;
+        let end = ((i + 1) * colsIn) as usize;
+        let row = &sliceIn[start..end];
+        input_data[i as usize] = row.to_vec();
+		//println!("{}, {}",row[0], row[1]);
+    }
+
+	let sliceOut = unsafe { std::slice::from_raw_parts(output, (rowsOut * colsOut) as usize) };
+    let mut output_data =  vec![vec![0.0; colsOut as usize]; rowsOut as usize];
+    for i in 0..rowsOut {
+        let start = (i * colsOut) as usize;
+        let end = ((i + 1) * colsOut) as usize;
+        let row = &sliceOut[start..end];
+        output_data[i as usize] = row.to_vec();
+    }
+
+    model.train(input_data, output_data, true, alpha, max_iter as usize, nb_errors as usize).leak().as_mut_ptr()
 }
